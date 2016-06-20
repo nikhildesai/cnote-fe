3727f4bd084bae8d9385db4d6c190c96f887ba37
Nikhil Desai <nikhil1234@gmail.com>
2016-06-19 17:32:51 -0700

    using cnote1.7 template

--- wp-modified-files/header.php
+++ wp-modified-files/header.php
@@ -16,6 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <link rel="profile" href="http://gmpg.org/xfn/11">
+    <!-- Bootstrap -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
 
@@ -23,6 +24,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/jquery.isonscreen.min.js"></script>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -30,33 +32,33 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-	
+
 </head>
 
 <body <?php body_class(); ?>>
-<div class="navigation main">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12 col-xs-12 col-sm-12">
-        <div class="row">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-           </button>
-           <div id="navbar" class="navbar-collapse collapse" aria-expanded="false">
-             <ul class="nav nav-justified">
-               <li><a href="/mission">Mission</a></li>
-               <li><a href="/blog">Blog</a></li>
-               <li><a href="/faq">FAQ</a></li>                                       
-             </ul>
-           </div>
-         </div>
-       </div>
-     </div>
-   </div>
- </div>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/"><img src="/images/mini-logo.png" alt=""></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="/about">About&nbsp;Us</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                    <li><a href="/faq">FAQ</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
 
  <div id="content" class="site-content">
-
